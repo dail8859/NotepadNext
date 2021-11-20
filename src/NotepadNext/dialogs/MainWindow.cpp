70504e81a864e07d8c997c9baa159ed1fb146277
dail8859 <dail8859@yahoo.com>
2021-11-20 12:18:59 -0500

    Prevent closing last file
    If a file was the last tab opened and it was closed it would end up with no tabs left

--- src/NotepadNext/dialogs/MainWindow.cpp
+++ src/NotepadNext/dialogs/MainWindow.cpp
@@ -802,6 +802,11 @@ void MainWindow::closeFile(ScintillaNext *editor)
 
     if(editor->isSavedToDisk()) {
         editor->close();
+
+        // If the last document was closed, start with a new one
+        if (dockedEditor->count() == 0) {
+            newFile();
+        }
     }
     else {
         // The user needs be asked what to do about this file, so switch to it
