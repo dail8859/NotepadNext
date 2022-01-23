b84ed0765549e60f29167e2772317fb0fa41d7c5
dail8859 <dail8859@yahoo.com>
2022-01-22 19:46:11 -0500

    Fix HTML language

--- src/NotepadNext/languages/html.lua
+++ src/NotepadNext/languages/html.lua
@@ -1,6 +1,6 @@
 local L = {}
 
-L.lexer = "html"
+L.lexer = "hypertext"
 
 L.extensions = {
 	"html",
@@ -13,15 +13,15 @@ L.extensions = {
 }
 
 L.keywords = {
-	[0] = "!doctype ^data- a abbr accept accept-charset accesskey acronym action address align alink alt applet archive area article aside async audio autocomplete autofocus axis b background base basefont bdi bdo bgcolor bgsound big blink blockquote body border br button canvas caption cellpadding cellspacing center char charoff charset checkbox checked cite class classid clear code codebase codetype col colgroup color cols colspan command compact content contenteditable contextmenu coords data datafld dataformatas datalist datapagesize datasrc datetime dd declare defer del details dfn dialog dir disabled div dl draggable dropzone dt element em embed enctype event face fieldset figcaption figure file font footer for form formaction formenctype formmethod formnovalidate formtarget frame frameborder frameset h1 h2 h3 h4 h5 h6 head header headers height hgroup hidden hr href hreflang hspace html http-equiv i id iframe image img input ins isindex ismap kbd keygen label lang language leftmargin legend li link list listing longdesc main manifest map marginheight marginwidth mark marquee max maxlength media menu menuitem meta meter method min multicol multiple name nav nobr noembed noframes nohref noresize noscript noshade novalidate nowrap object ol onabort onafterprint onautocomplete onautocompleteerror onbeforeonload onbeforeprint onblur oncancel oncanplay oncanplaythrough onchange onclick onclose oncontextmenu oncuechange ondblclick ondrag ondragend ondragenter ondragleave ondragover ondragstart ondrop ondurationchange onemptied onended onerror onfocus onhashchange oninput oninvalid onkeydown onkeypress onkeyup onload onloadeddata onloadedmetadata onloadstart onmessage onmousedown onmouseenter onmouseleave onmousemove onmouseout onmouseover onmouseup onmousewheel onoffline ononline onpagehide onpageshow onpause onplay onplaying onpointercancel onpointerdown onpointerenter onpointerleave onpointerlockchange onpointerlockerror onpointermove onpointerout onpointerover onpointerup onpopstate onprogress onratechange onreadystatechange onredo onreset onresize onscroll onseeked onseeking onselect onshow onsort onstalled onstorage onsubmit onsuspend ontimeupdate ontoggle onundo onunload onvolumechange onwaiting optgroup option output p param password pattern picture placeholder plaintext pre profile progress prompt public q radio readonly rel required reset rev reversed role rows rowspan rp rt rtc ruby rules s samp sandbox scheme scope scoped script seamless section select selected shadow shape size sizes small source spacer span spellcheck src srcdoc standby start step strike strong style sub submit summary sup svg svg:svg tabindex table target tbody td template text textarea tfoot th thead time title topmargin tr track tt type u ul usemap valign value valuetype var version video vlink vspace wbr width xml xmlns xmp",
+	[0] = "^data- a abbr accept accept-charset accesskey acronym action address align alink alt applet archive area article aside async audio autocomplete autofocus axis b background base basefont bdi bdo bgcolor bgsound big blink blockquote body border br button canvas caption cellpadding cellspacing center char charoff charset checkbox checked cite class classid clear code codebase codetype col colgroup color cols colspan command compact content contenteditable contextmenu coords data datafld dataformatas datalist datapagesize datasrc datetime dd declare defer del details dfn dialog dir disabled div dl draggable dropzone dt element em embed enctype event face fieldset figcaption figure file font footer for form formaction formenctype formmethod formnovalidate formtarget frame frameborder frameset h1 h2 h3 h4 h5 h6 head header headers height hgroup hidden hr href hreflang hspace html http-equiv i id iframe image img input ins isindex ismap kbd keygen label lang language leftmargin legend li link list listing longdesc main manifest map marginheight marginwidth mark marquee max maxlength media menu menuitem meta meter method min multicol multiple name nav nobr noembed noframes nohref noresize noscript noshade novalidate nowrap object ol onabort onafterprint onautocomplete onautocompleteerror onbeforeonload onbeforeprint onblur oncancel oncanplay oncanplaythrough onchange onclick onclose oncontextmenu oncuechange ondblclick ondrag ondragend ondragenter ondragleave ondragover ondragstart ondrop ondurationchange onemptied onended onerror onfocus onhashchange oninput oninvalid onkeydown onkeypress onkeyup onload onloadeddata onloadedmetadata onloadstart onmessage onmousedown onmouseenter onmouseleave onmousemove onmouseout onmouseover onmouseup onmousewheel onoffline ononline onpagehide onpageshow onpause onplay onplaying onpointercancel onpointerdown onpointerenter onpointerleave onpointerlockchange onpointerlockerror onpointermove onpointerout onpointerover onpointerup onpopstate onprogress onratechange onreadystatechange onredo onreset onresize onscroll onseeked onseeking onselect onshow onsort onstalled onstorage onsubmit onsuspend ontimeupdate ontoggle onundo onunload onvolumechange onwaiting optgroup option output p param password pattern picture placeholder plaintext pre profile progress prompt public q radio readonly rel required reset rev reversed role rows rowspan rp rt rtc ruby rules s samp sandbox scheme scope scoped script seamless section select selected shadow shape size sizes small source spacer span spellcheck src srcdoc standby start step strike strong style sub submit summary sup svg svg:svg tabindex table target tbody td template text textarea tfoot th thead time title topmargin tr track tt type u ul usemap valign value valuetype var version video vlink vspace wbr width xml xmlns xmp",
+	[5] = "ELEMENT DOCTYPE ATTLIST ENTITY NOTATION",
 }
 
 L.styles = {
 	["DEFAULT"] = {
 		id = 0,
 		fgColor = rgb(0x000000),
-		bgColor = rgb(0xFFFFFF),
-		fontStyle = 1,
+		bgColor = rgb(0xFFFFFF)
 	},
 	["COMMENT"] = {
 		id = 9,
@@ -70,11 +70,6 @@ L.styles = {
 		fgColor = rgb(0x000000),
 		bgColor = rgb(0xFFFFFF),
 	},
-	["SGMLDEFAULT"] = {
-		id = 21,
-		fgColor = rgb(0x000000),
-		bgColor = rgb(0xA6CAF0),
-	},
 	["CDATA"] = {
 		id = 17,
 		fgColor = rgb(0xFF8000),
@@ -90,6 +85,36 @@ L.styles = {
 		fgColor = rgb(0x000000),
 		bgColor = rgb(0xFEFDE0),
 		fontStyle = 2,
+	},
+	["SGML_DEFAULT"] = {
+		id = 21,
+		fgColor = rgb(0x0000FF),
+		bgColor = rgb(0xFFFFFF),
+		fontStyle = 1,
+	},
+	["SGML_COMMAND"] = {
+		id = 22,
+		fgColor = rgb(0x0000FF),
+		bgColor = rgb(0xFFFFFF),
+		fontStyle = 1,
+	},
+	["SGML_PARAM"] = {
+		id = 23,
+		fgColor = rgb(0xFF0000),
+		bgColor = rgb(0xFFFFFF),
+		fontStyle = 1,
+	},
+	["SGML_DOUBLESTRING"] = {
+		id = 24,
+		fgColor = rgb(0x808080),
+		bgColor = rgb(0xFFFFFF),
+		fontStyle = 1,
+	},
+	["SGML_COMMENT"] = {
+		id = 29,
+		fgColor = rgb(0x008000),
+		bgColor = rgb(0xFFFFFF),
+		fontStyle = 1,
 	},
 }
 return L
