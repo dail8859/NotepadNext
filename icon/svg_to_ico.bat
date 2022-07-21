00e9239d5a612d94e8654e8594fd3080a106a7f9
dail8859 <dail8859@yahoo.com>
2022-07-21 18:49:23 -0400

    Add larger icon for Start Menu on Windows
    Closes #176

--- icon/svg_to_ico.bat
+++ icon/svg_to_ico.bat
@@ -3,13 +3,15 @@
 "C:\Program Files\Inkscape\bin\inkscape.exe" -w 48 -h 48 --export-type="png" --export-filename="nn48.png" NotepadNext.svg
 "C:\Program Files\Inkscape\bin\inkscape.exe" -w 64 -h 64 --export-type="png" --export-filename="nn64.png" NotepadNext.svg
 "C:\Program Files\Inkscape\bin\inkscape.exe" -w 128 -h 128 --export-type="png" --export-filename="nn128.png" NotepadNext.svg
-".\ImageMagick\magick" convert -background transparent nn16.png nn32.png nn48.png nn64.png nn128.png NotepadNext.ico
+"C:\Program Files\Inkscape\bin\inkscape.exe" -w 128 -h 128 --export-type="png" --export-filename="nn512.png" NotepadNext.svg
+".\ImageMagick\magick" convert -background transparent nn16.png nn32.png nn48.png nn64.png nn128.png nn512.png NotepadNext.ico
 
 del nn16.png
 del nn32.png
 del nn48.png
 del nn64.png
 del nn128.png
+del nn512.png
 
 "C:\Program Files\Inkscape\bin\inkscape.exe" --export-type="png" --export-dpi=96 --export-background-opacity=0 NotepadNext.svg
 
