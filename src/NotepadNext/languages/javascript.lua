93715aebc92c11f3fa83837cb65df4c1e8629435
dail8859 <dail8859@yahoo.com>
2022-09-28 21:50:48 -0400

    Fix Javascript language
    Closes #209

--- src/NotepadNext/languages/javascript.lua
+++ src/NotepadNext/languages/javascript.lua
@@ -1,70 +1,117 @@
 local L = {}
 
-L.lexer = "javascript"
+L.lexer = "cpp"
 
 L.singleLineComment = "// "
 
+L.extensions = {
+    "js",
+    "jsm",
+    "jsx",
+    "ts",
+    "tsx",
+}
+
+L.properties = {
+    ["lexer.cpp.backquoted.strings"] = "1",
+}
+
 L.keywords = {
-	[0] = "abstract async await boolean break byte case catch char class const continue debugger default delete do double else enum export extends final finally float for function goto if implements import in instanceof int interface let long native new null of package private protected public return short static super switch synchronized this throw throws transient try typeof var void volatile while with true false prototype yield",
+    [0] = "abstract async await boolean break byte case catch char class const continue debugger default delete do double else enum export extends final finally float for function goto if implements import in instanceof int interface let long native new null of package private protected public return short static super switch synchronized this throw throws transient try typeof var void volatile while with true false prototype yield",
+    [1] = "Array Date eval hasOwnProperty Infinity isFinite isNaN isPrototypeOf Math NaN Number Object prototype String toString undefined valueOf",
+    [3] = "alert all anchor anchors area assign blur button checkbox clearInterval clearTimeout clientInformation close closed confirm constructor crypto decodeURI decodeURIComponent defaultStatus document element elements embed embeds encodeURI encodeURIComponent escape event fileUpload focus form forms frame innerHeight innerWidth layer layers link location mimeTypes navigate navigator frames frameRate hidden history image images offscreenBuffering onblur onclick onerror onfocus onkeydown onkeypress onkeyup onmouseover onload onmouseup onmousedown onsubmit open opener option outerHeight outerWidth packages pageXOffset pageYOffset parent parseFloat parseInt password pkcs11 plugin prompt propertyIsEnum radio reset screenX screenY scroll secure select self setInterval setTimeout status submit taint text textarea top unescape untaint window",
 }
 
 L.styles = {
-	["DEFAULT"] = {
-		id = 41,
-		fgColor = rgb(0x000000),
-		bgColor = rgb(0xF2F4FF),
-	},
-	["NUMBER"] = {
-		id = 45,
-		fgColor = rgb(0xFF0000),
-		bgColor = rgb(0xF2F4FF),
-	},
-	["WORD"] = {
-		id = 46,
-		fgColor = rgb(0x000000),
-		bgColor = rgb(0xF2F4FF),
-	},
-	["KEYWORD"] = {
-		id = 47,
-		fgColor = rgb(0x000080),
-		bgColor = rgb(0xF2F4FF),
-		fontStyle = 3,
-	},
-	["DOUBLESTRING"] = {
-		id = 48,
-		fgColor = rgb(0x808080),
-		bgColor = rgb(0xF2F4FF),
-	},
-	["SINGLESTRING"] = {
-		id = 49,
-		fgColor = rgb(0x808080),
-		bgColor = rgb(0xF2F4FF),
-	},
-	["SYMBOLS"] = {
-		id = 50,
-		fgColor = rgb(0x000000),
-		bgColor = rgb(0xF2F4FF),
-		fontStyle = 1,
-	},
-	["REGEX"] = {
-		id = 52,
-		fgColor = rgb(0x8000FF),
-		bgColor = rgb(0xF2F4FF),
-	},
-	["COMMENT"] = {
-		id = 42,
-		fgColor = rgb(0x008000),
-		bgColor = rgb(0xF2F4FF),
-	},
-	["COMMENTLINE"] = {
-		id = 43,
-		fgColor = rgb(0x008000),
-		bgColor = rgb(0xF2F4FF),
-	},
-	["COMMENTDOC"] = {
-		id = 44,
-		fgColor = rgb(0x008080),
-		bgColor = rgb(0xF2F4FF),
-	},
+    ["DEFAULT"] = {
+        id = 11,
+        fgColor = rgb(0x000000),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["INSTRUCTION WORD"] = {
+        id = 5,
+        fgColor = rgb(0x0000FF),
+        bgColor = rgb(0xFFFFFF),
+        fontStyle = 1,
+    },
+    ["TYPE WORD"] = {
+        id = 16,
+        fgColor = rgb(0x8000FF),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["WINDOW INSTRUCTION"] = {
+        id = 19,
+        fgColor = rgb(0x804000),
+        bgColor = rgb(0xFFFFFF),
+        fontStyle = 1,
+    },
+    ["NUMBER"] = {
+        id = 4,
+        fgColor = rgb(0xFF8000),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["STRING"] = {
+        id = 6,
+        fgColor = rgb(0x808080),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["STRINGRAW"] = {
+        id = 20,
+        fgColor = rgb(0x000080),
+        bgColor = rgb(0xC0C0C0),
+    },
+    ["CHARACTER"] = {
+        id = 7,
+        fgColor = rgb(0x808080),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["OPERATOR"] = {
+        id = 10,
+        fgColor = rgb(0x000080),
+        bgColor = rgb(0xFFFFFF),
+        fontStyle = 1,
+    },
+    ["VERBATIM"] = {
+        id = 13,
+        fgColor = rgb(0x000000),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["REGEX"] = {
+        id = 14,
+        fgColor = rgb(0x000000),
+        bgColor = rgb(0xFFFFFF),
+        fontStyle = 1,
+    },
+    ["COMMENT"] = {
+        id = 1,
+        fgColor = rgb(0x008000),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["COMMENT LINE"] = {
+        id = 2,
+        fgColor = rgb(0x008000),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["COMMENT DOC"] = {
+        id = 3,
+        fgColor = rgb(0x008080),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["COMMENT LINE DOC"] = {
+        id = 15,
+        fgColor = rgb(0x008080),
+        bgColor = rgb(0xFFFFFF),
+    },
+    ["COMMENT DOC KEYWORD"] = {
+        id = 17,
+        fgColor = rgb(0x008080),
+        bgColor = rgb(0xFFFFFF),
+        fontStyle = 1,
+    },
+    ["COMMENT DOC KEYWORD ERROR"] = {
+        id = 18,
+        fgColor = rgb(0x008080),
+        bgColor = rgb(0xFFFFFF),
+    },
 }
 return L
