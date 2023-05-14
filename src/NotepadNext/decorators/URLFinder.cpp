faa24febab1affa004ed91ea976068201a33004b
chengzhl <chengzhl@cisco.com>
2023-05-14 08:40:40 +0800

    Revert "simplify code"
    This reverts commit dae94f2f0361907adcc4f613dc62edc7694bdceb.

--- src/NotepadNext/decorators/URLFinder.cpp
+++ src/NotepadNext/decorators/URLFinder.cpp
@@ -66,9 +66,11 @@ void URLFinder::findURLs()
         const int endPos = editor->lineEndPosition(currentLine);
         const QByteArray lineText = editor->get_text_range(startPos, endPos);
 
-        static QRegularExpression re(R"(\bhttps?://[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*))");
+        static QRegularExpression regex(R"(\bhttps?://[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*))");
         QStringList matchedTexts;
-        for (const QRegularExpressionMatch &match : re.globalMatch(lineText)) {
+        QRegularExpressionMatchIterator iterator = regex.globalMatch(lineText);
+        while (iterator.hasNext()) {
+            QRegularExpressionMatch match = iterator.next();
             const QString matched = match.captured(0);
             if (!matched.isEmpty() && !matchedTexts.contains(matched)) {
                 matchedTexts.append(matched);
