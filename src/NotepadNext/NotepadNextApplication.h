39cabe5dd85398b8cd0aec5d8c27ec93c66a2745
wayne_zhang <wayne.zhang.au@gmail.com>
2023-07-16 00:16:12 +0800

    setLanguageColors method doc

--- src/NotepadNext/NotepadNextApplication.h
+++ src/NotepadNext/NotepadNextApplication.h
@@ -64,7 +64,8 @@ public:
     void loadTranslation(QLocale locale);
 
     void sendInfoToPrimaryInstance();
-
+    
+    // expose this method to make it possible to change theme without restart
     void setLanguageColors();
 
 protected:
