bbe7222ab7ca20f9d4b7a5b784e0bea0f057249a
dail8859 <dail8859@yahoo.com>
2024-09-24 22:22:15 -0400

    Fix license wrapping

--- src/NotepadNext/dialogs/MainWindow.cpp
+++ src/NotepadNext/dialogs/MainWindow.cpp
@@ -709,16 +709,7 @@ MainWindow::MainWindow(NotepadNextApplication *app) :
                             QStringLiteral("<h3>%1 v%2%3</h3>"
                                     "<p>%4</p>"
                                     "<p>This program does stuff.</p>"
-                                    R"(<p>This program is free software: you can redistribute it and/or modify
-                                    it under the terms of the GNU General Public License as published by
-                                    the Free Software Foundation, either version 3 of the License, or
-                                    (at your option) any later version.</p>
-                                    <p>This program is distributed in the hope that it will be useful,
-                                    but WITHOUT ANY WARRANTY; without even the implied warranty of
-                                    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-                                    GNU General Public License for more details.</p>
-                                    <p>You should have received a copy of the GNU General Public License
-                                    along with this program. If not, see &lt;<a href="https://www.gnu.org/licenses/">https://www.gnu.org/licenses/</a>&gt;.</p>)")
+                                    R"(<p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p> <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.</p> <p>You should have received a copy of the GNU General Public License along with this program. If not, see &lt;<a href="https://www.gnu.org/licenses/">https://www.gnu.org/licenses/</a>&gt;.</p>)")
                                 .arg(QApplication::applicationDisplayName(), APP_VERSION, APP_DISTRIBUTION, QStringLiteral(APP_COPYRIGHT).toHtmlEscaped()));
     });
 
