f2b11f4d28d22dafe1697bdaa109c93a4a220bf6
dail8859 <dail8859@yahoo.com>
2024-11-29 14:00:30 -0500

    Add link to Github page
    Closes #671

--- src/NotepadNext/dialogs/MainWindow.cpp
+++ src/NotepadNext/dialogs/MainWindow.cpp
@@ -706,9 +706,9 @@ MainWindow::MainWindow(NotepadNextApplication *app) :
     ui->actionAboutNotepadNext->setShortcut(QKeySequence::HelpContents);
     connect(ui->actionAboutNotepadNext, &QAction::triggered, this, [=]() {
         QMessageBox::about(this, QString(),
-                            QStringLiteral("<h3>%1 v%2%3</h3>"
+                            QStringLiteral("<h3>%1 v%2 %3</h3>"
                                     "<p>%4</p>"
-                                    "<p>This program does stuff.</p>"
+                                    "<p><a href=\"https://github.com/dail8859/NotepadNext\">Notepad Next Home Page</a></p>"
                                     R"(<p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p> <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.</p> <p>You should have received a copy of the GNU General Public License along with this program. If not, see &lt;<a href="https://www.gnu.org/licenses/">https://www.gnu.org/licenses/</a>&gt;.</p>)")
                                 .arg(QApplication::applicationDisplayName(), APP_VERSION, APP_DISTRIBUTION, QStringLiteral(APP_COPYRIGHT).toHtmlEscaped()));
     });
