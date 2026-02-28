a688246e94ddbe126db9f9e72e123fee57ae8947
dail8859 <dail8859@yahoo.com>
2026-02-28 12:06:23 -0500

    Update .ico file with 256 layer

--- icon/svg_to_ico.bat
+++ icon/svg_to_ico.bat
@@ -1,20 +1,15 @@
-"C:\Program Files\Inkscape\bin\inkscape.exe" -w 16 -h 16 --export-type="png" --export-filename="nn16.png" NotepadNext.svg
-"C:\Program Files\Inkscape\bin\inkscape.exe" -w 32 -h 32 --export-type="png" --export-filename="nn32.png" NotepadNext.svg
-"C:\Program Files\Inkscape\bin\inkscape.exe" -w 48 -h 48 --export-type="png" --export-filename="nn48.png" NotepadNext.svg
-"C:\Program Files\Inkscape\bin\inkscape.exe" -w 64 -h 64 --export-type="png" --export-filename="nn64.png" NotepadNext.svg
-"C:\Program Files\Inkscape\bin\inkscape.exe" -w 128 -h 128 --export-type="png" --export-filename="nn128.png" NotepadNext.svg
-"C:\Program Files\Inkscape\bin\inkscape.exe" -w 128 -h 128 --export-type="png" --export-filename="nn512.png" NotepadNext.svg
-".\ImageMagick\magick" convert -background transparent nn16.png nn32.png nn48.png nn64.png nn128.png nn512.png NotepadNext.ico
+@echo off
+set ICON_SIZES=16 32 48 64 128 256
 
-del nn16.png
-del nn32.png
-del nn48.png
-del nn64.png
-del nn128.png
-del nn512.png
+for %%s in (%ICON_SIZES%) do (
+    echo Exporting %%sx%%s PNG...
+    "C:\Program Files\Inkscape\bin\inkscape.exe" NotepadNext.svg -w %%s -h %%s --export-type=png --export-filename=nn%%s.png
+)
 
-"C:\Program Files\Inkscape\bin\inkscape.exe" --export-type="png" --export-dpi=96 --export-background-opacity=0 NotepadNext.svg
+".\ImageMagick\magick" convert -background transparent nn16.png nn32.png nn48.png nn64.png nn128.png nn256.png NotepadNext.ico
 
+for %%s in (%ICON_SIZES%) do del nn%%s.png
+
+"C:\Program Files\Inkscape\bin\inkscape.exe" NotepadNext.svg --export-type=png --export-dpi=96 --export-background-opacity=0 
 ".\png2icons.exe" NotepadNext.png NotepadNext -icns
-
 copy NotepadNext.png ..\src\NotepadNext\icons\NotepadNext.png
