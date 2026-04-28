3dae5d1ec28116f5b43a03ded5a8b06d5999ce04
Hugo Carvalho <hugokarvalho@hotmail.com>
2026-04-28 10:47:17 +0100

    Add files via upload

--- i18n/NotepadNext_pt.ts
+++ i18n/NotepadNext_pt.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt" sourcelanguage="en_US">
+<TS version="2.1" language="pt" sourcelanguage="en">
 <context>
     <name>ColumnEditorDialog</name>
     <message>
