dail8859/NotepadNext

commit b4c6f51586bd5567a6e817e2932ad3b577cf3951

Limit zoom out

src/dialogs/MainWindow.cpp
@@ -712,6 +712,9 @@ MainWindow::MainWindow(NotepadNextApplication *app) :
         showEditorZoomLevelIndicator();
     });
     connect(ui->actionZoomOut, &QAction::triggered, this, [this]() {
+        // Scintilla can zoom out to "-10" but on a screen with fractional scaling it throws alot of Qt warnings
+        if (zoomLevel == -9) return;
+
         for (ScintillaNext *editor : editors()) {
             editor->zoomOut();
         }

commit 29fe94b1c779675dbd9b8a206a831d24270b39b5

Fix spelling error in zoom out comment
The codespell CI check has been failing on master since b4c6f51 because
of this typo.

src/dialogs/MainWindow.cpp
@@ -739,7 +739,7 @@ MainWindow::MainWindow(NotepadNextApplication *app) :
         showEditorZoomLevelIndicator();
     });
     connect(ui->actionZoomOut, &QAction::triggered, this, [this]() {
-        // Scintilla can zoom out to "-10" but on a screen with fractional scaling it throws alot of Qt warnings
+        // Scintilla can zoom out to "-10" but on a screen with fractional scaling it throws a lot of Qt warnings
         if (zoomLevel == -9) return;
 
         for (ScintillaNext *editor : editors()) {